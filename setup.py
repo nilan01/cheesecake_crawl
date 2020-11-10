53632eff43c3bc0e9969a9176a2ddf5f21177b6e
Chris Pinkney <hey@chrispinkney.com>
2020-11-10 10:20:30 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="cheese scraper",
+    version="0.1",
+    author="Chris Pinkney",
+    author_email="hey@chrispinkney.com",
+    install_requires=[
+        "bs4", "requests"
+    ],
+    entry_points={"console_scripts": ["cheese scraper = main : main"]},
+)
